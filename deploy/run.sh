661867bc1fd28c19b9e8a8e7f65315e9416dde4a
soichih <soichih@15b46722-cb4c-11de-b15d-2770122ffd34>
2010-03-08 04:12:56 +0000

    Updated DivRep lib (Data locale fix) Updated run.sh to use java specified in JAVA_HOME

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,1 +1,8 @@
-java -jar dsbudget.jar 
+#!/bin/bash
+if [ ! $JAVA_HOME ]; then
+	echo "JAVA_HOME is not set.. please install SUN Java 6 (not Open-JDK!), then add something like following to your ~/.profile"
+	echo "export JAVA_HOME=/usr/lib/jvm/default-java"
+	exit
+fi
+
+$JAVA_HOME/bin/java -jar dsbudget.jar &
