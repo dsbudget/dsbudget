cec3c99778201e59948c271d38fe451c5b88cc1d
soichih <soichih@15b46722-cb4c-11de-b15d-2770122ffd34>
2010-04-18 03:49:37 +0000

    removing /build under deploy

--- deploy/build/dsbudget/tomcat/webapps/dsbudget/dsbudget.js
+++
@@ -1,9 +0,0 @@
-//http://lanitdev.wordpress.com/2009/07/23/make-table-rows-sortable-using-jquery-ui-sortable/
-/*
-var divrep_tablesortablehelper = function(e, ui) {
-	ui.children().each(function() {
-		$(this).width($(this).width());
-	});
-	return ui;
-};
-*/
