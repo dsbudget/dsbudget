8f5a37e2d0d55069361a9ab98e9e10ab45862d29
soichih <soichih@15b46722-cb4c-11de-b15d-2770122ffd34>
2010-08-08 16:05:37 +0000

    2.0.21

--- src/dsbudget/Main.java
+++ src/dsbudget/Main.java
@@ -34,7 +34,7 @@ import dsbudget.model.Page;
 public class Main {
 	static Logger logger = Logger.getLogger(Main.class);
 	
-	static public String version = "2.0.20";
+	static public String version = "2.0.21";
 	
     public static Embedded tomcat = null;
     private Host host = null;
